ae811311626de4ec32663150162f2f259417f3d9
round2fight <157293187+round2fight@users.noreply.github.com>
2024-10-09 14:45:28 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -20,9 +20,9 @@ module.exports = {
   },
   production: {
     username: "voosh_database_user",
-    password: "8vNMKgI82W6pWP0L6yyCpUE3QNPnVwlp",
-    database: "voosh_database",
-    host: "dpg-cqekdppu0jms739e4cag-a", //"127.0.0.1",
+    password: "5VA8T9t6LnShzfmHVVGgEeo52hYxz6zq",
+    database: "voosh_database_fc3z",
+    host: "dpg-cs3470t6l47c73e9a8jg-a", //"127.0.0.1",
     port: "5432",
     dialect: "postgres",
   },
